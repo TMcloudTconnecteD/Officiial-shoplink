a5173a4fcc24540b4b130b6377ac892a7049835f
THURANIRA <larrygerald76@gmail.com>
2025-04-04 17:27:36 +0300

    dist for test

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import connectDB from './config/db.js';
-
-
 import userRoutes from './routes/userRoutes.js';
-
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -15,11 +12,20 @@ console.log("MONGO_URI:", process.env.MONGO_URI);
 
 connectDB();
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use('/api/users', userRoutes)
+// API routes
+app.use('/api/users', userRoutes);
 
-app.listen(port, () => console.log(`server is running on port: ${port}`))
+// Serve frontend
+const __dirname = path.resolve(); // Required for resolving paths with ESModules
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`server is running on port: ${port}`));

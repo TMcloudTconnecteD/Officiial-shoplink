4a8f64846b9af73e96d1c9599bb41fefd023f44d
THURANIRA <larrygerald76@gmail.com>
2025-04-20 23:29:31 +0300

    render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import WindiCSS from 'vite-plugin-windicss'
 
-export default defineConfig({
-  plugins: [react(), WindiCSS()],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api': 'http://localhost:8000',
-      '/uploads': 'http://localhost:8000',
+export default ({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [react(), WindiCSS()],
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/api': env.VITE_BASE_URL || 'http://localhost:8000',
+        '/uploads': env.VITE_BASE_URL || 'http://localhost:8000',
+      },
+      allowedHosts: [
+        "https://5a78-129-222-187-227.ngrok-free.app", // current ngrok domain
+        '.ngrok-free.app',                     // wildcard for future ngrok links
+      ],
     },
-    allowedHosts: [
-      "    https://5a78-129-222-187-227.ngrok-free.app ", // current ngrok domain
-      '.ngrok-free.app',                     // wildcard for future ngrok links
-    ],
-  },
-})
+  })
+}

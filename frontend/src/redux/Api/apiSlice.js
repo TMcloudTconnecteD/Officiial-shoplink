7cc57ab96f912f04c46b5577c5f30e5d2290b4aa
THURANIRA <larrygerald76@gmail.com>
2025-05-19 09:54:10 +0300

    render3.o

--- frontend/src/redux/Api/apiSlice.js
+++ frontend/src/redux/Api/apiSlice.js
@@ -3,8 +3,7 @@ import { BASE_URL } from '../features/constants.js';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: BASE_URL,
-    credentials: 'include', 
- // Add this line to send cookies with requests
+    credentials: 'include', // Add this line to send cookies with requests
     prepareHeaders: (headers, { getState }) => {
         // Get the token from the Redux state or localStorage
         const token = getState().auth?.token || localStorage.getItem('token');

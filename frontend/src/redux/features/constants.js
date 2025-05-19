352483e6a256addf6e73efd5c7cc66f9cfab3168
THURANIRA <larrygerald76@gmail.com>
2025-05-19 09:34:51 +0300

    frontend render2.o

--- frontend/src/redux/features/constants.js
+++ frontend/src/redux/features/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || '' //proxy
+export const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://shoplink-b.onrender.com' //proxy
 export const USERS_URL = '/api/users'
 export const CATEGORY_URL = '/api/category'
 export const PRODUCT_URL = '/api/products'

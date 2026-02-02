ee6a558f707a60da6fe32455ed5575ac2bae9118
GitHub Copilot <larrygerald76@gmail.com>
2026-02-02 20:24:00 +0300

    render fall

--- backend/middlewares/authMiddlewares.js
+++ backend/middlewares/authMiddlewares.js
@@ -82,4 +82,4 @@ const authorizeSuperAdmin = (req, res, next) => {
     }
 }
 
-export { authenticate, authorizeAdmin, authorizeSuperAdmin };
+export { authenticate, authorizeAdmin, authorizeSuperAdmin, optionalAuthenticate };
